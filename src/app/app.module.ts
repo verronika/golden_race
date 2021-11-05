81cd0133761cf7615f811a168ca3c08d4fe947c1
Veronica <veronicamarting191@gmail.com>
2021-11-05 19:36:13 +0100

    2 colums

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatCardModule } from '@angular/material/card';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatButtonModule} from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
 
 @NgModule({
   declarations: [
@@ -12,9 +15,30 @@ import { MatCardModule } from '@angular/material/card';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MatCardModule
+    MatCardModule,
+    MatGridListModule,
+    MatButtonModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+export interface Tile {
+  cols: number;
+  rows: number;
+}
+
+/**
+ *  Dynamic grid-list
+ */
+
+export class GridListDynamicExample {
+  tiles: Tile[] = [
+    {cols: 3, rows: 1},
+    {cols: 1, rows: 2},
+    {cols: 1, rows: 1},
+    {cols: 2, rows: 1},
+  ];
+}
