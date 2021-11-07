304c236bfd06c0dc85dc6b84e17ed24dd10b8d22
Veronica <veronicamarting191@gmail.com>
2021-11-07 13:11:20 +0100

    click balls

--- src/app/ball-selector/ball-selector.component.ts
+++ src/app/ball-selector/ball-selector.component.ts
@@ -7,9 +7,56 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BallSelectorComponent implements OnInit {
 
+  balls: Array<any> = [
+    {
+      number: 1,
+      color: 'primary'
+    },
+    {
+      number: 2,
+      color: 'accent'
+    },
+    {
+      number: 3,
+      color: 'warn'
+    },
+    {
+      number: 4,
+      color: 'primary'
+    },
+    {
+      number: 5,
+      color: 'warn'
+    },
+    {
+      number: 6,
+      color: 'primary'
+    },
+    {
+      number: 7,
+      color: 'accent'
+    },
+    {
+      number: 8,
+      color: 'warn'
+    },
+    {
+      number: 9,
+      color: 'primary'
+    },
+    {
+      number: 10,
+      color: 'accent'
+    },
+  ];
+  
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  clickBall(event: any, numberSelection: number) {
+    console.log(numberSelection);
   }
 
 }
